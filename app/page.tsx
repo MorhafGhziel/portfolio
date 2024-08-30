1d7978f42fab9eb3ce15a4a12bc176bebfcd74db
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-30 21:26:07 +0300

    added nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -9,6 +11,9 @@ export default function Home() {
     >
       <div className="max-w-7xl w-full">
         <Hero />
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
       </div>
     </main>
   );
