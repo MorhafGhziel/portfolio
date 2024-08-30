a894897563ee456bfa9461e7932c3f303e3f549f
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-30 21:51:31 +0300

    created the first card

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,20 @@
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
-  return <section id="about"></section>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {[{ title: "Title1", description: "desc1", id: 1 }].map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
