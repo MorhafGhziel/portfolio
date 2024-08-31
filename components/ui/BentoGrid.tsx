e0e6ec8bd8561851e50a3d0bedde031efebb5264
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-08-31 16:05:23 +0300

    styled the globe

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -46,7 +46,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl relative group/bento p-4 hover:shadow-xl transition duration-200 shadow-input dark:shadow-none dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-3xl relative group/bento p-4 hover:shadow-xl overflow-hidden transition duration-200 shadow-input dark:shadow-none dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
