dc41aa4666dafd8677f90d5e49cacf80ad6108e7
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-01 15:03:12 +0300

    changed the projects alt title

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -5,21 +5,21 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20 ">
+    <div className="py-20">
       <h1 className="heading">
-        A small selection of {""}
-        <span className="text-purple">rescent projects</span>
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className=" sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
-            <PinContainer title={title} href={link}>
+            <PinContainer title="Visit" href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D] ">
-                  <img src="/bg.png" alt="bg img" />
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
+                  <img src="/bg.png" alt="Background image" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
@@ -36,21 +36,21 @@ const RecentProjects = () => {
                   {iconLists.map((icon, index) => (
                     <div
                       key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      className="border border-white/20 rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <img src={icon} alt="icon" className="p-2" />
                     </div>
                   ))}
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple ">
+                  <p className="flex lg:text-xl md:text-sm text-xs text-purple">
                     Check Live Site
                   </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  <FaLocationArrow className="ml-3" color="#CBACF9" />
                 </div>
               </div>
             </PinContainer>
