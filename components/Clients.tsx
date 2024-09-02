3c49386e87123eca2c19236d668dbe7f19264149
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-02 18:33:01 +0300

    added the clinets section

---
+++ components/Clients.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <div className="py-20" id="">
+      <h1 className="heading">
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Clients;
