749d7a811efaa9c060898d1f4135ca43d367684f
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-02 20:37:46 +0300

    added the experience section

---
+++ components/Experience.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <section id="testimonials" className="py-20">
+      <h1 className="heading">
+        My
+        <span className="text-purple"> work experience</span>
+      </h1>
+    </section>
+  );
+};
+
+export default Experience;
