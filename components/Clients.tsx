989f5364091036877a732561175fb79c5ac97f19
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-02 18:53:17 +0300

    styled the cards

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -9,12 +9,14 @@ const Clients = () => {
         Kind words from
         <span className="text-purple"> satisfied clients</span>
       </h1>
-      <div className="flex flex-col items-center">
-        <InfiniteMovingCards
-          items={testimonials}
-          direction="right"
-          speed="slow"
-        />
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
       </div>
     </div>
   );
