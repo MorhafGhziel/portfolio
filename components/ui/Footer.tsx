cd86034a30a2b2c9aa09d8f3a527111422dd637a
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-03 15:13:28 +0300

    added links to the socialmedia icons

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,12 +1,10 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
@@ -38,14 +36,28 @@ const Footer = () => {
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-3"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-          ))}
+          {socialMedia.map((info) => {
+            let link = "";
+            if (info.id === 1) {
+              link = "https://github.com/MorhafGhziel";
+            } else if (info.id === 2) {
+              link = "/";
+            } else if (info.id === 3) {
+              link = "https://www.linkedin.com/in/morhaf-ghziel-a720a72b9/";
+            }
+
+            return link ? (
+              <a
+                key={info.id}
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-3"
+              >
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </a>
+            ) : null;
+          })}
         </div>
       </div>
     </footer>
